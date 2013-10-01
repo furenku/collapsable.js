40a6abc8a027fd1e120d00181b7c66bd66023860
furenku <furenku@tim.imagen-movimiento.org>
2013-10-01 17:43:03 -0500

    dejar el ultimo siempre abierto, y su titulo oculto

--- collapsable.js
+++ collapsable.js
@@ -54,14 +54,31 @@ Cortina = function( parent ){
     this.collapse = function(){
 	// var child = this.contenido.find('.pad_div');
 	// child.animate({width: this.contenido.width() },1000);
-        this.contenido.animate(
-            { width: 0 },
-            1000,
-            function(){
-                this.collapsed = true;
-            }
-        );
-	this.titulo.animate({left:0},1000);
+	var collapsables = this.parent.cortinas;
+	var index = $.inArray( this, collapsables );
+	if( index != collapsables.length - 1 ) {
+	    var all_collapsed = true;
+	    
+            this.contenido.animate(
+		{ width: 0 },
+		1000,
+		function(){
+                    this.collapsed = true;
+		    /*
+		      for( i in collapsables ) {
+		      if( collapsables[i].collapsed ) {
+		      all_collapsed = false;
+		      break;
+		      }
+		      }
+		      if ( all_collapsed ) {
+		      collapsables[ collapsables.length - 1 ].expand();
+		      }
+		    */		
+		}
+            );
+	    this.titulo.animate({left:0},1000);
+	}
     }
 
     this.expand = function(){
@@ -70,7 +87,7 @@ Cortina = function( parent ){
 	var totalW = 0;
 
 	var lastTtlW = 0;
-	
+
 	for( i in collapsables ) {
 	    if( i != index ) {
 		collapsables[i].collapse();
@@ -90,35 +107,51 @@ Cortina = function( parent ){
 
 	} else {
 	    for( i in collapsables ) {
-		totalW += collapsables[i].titulo.width(); 
+		if(i<collapsables.length-1) {
+		    totalW += collapsables[i].titulo.width(); 
+		}
 	    }
 	}
 
-	console.log( 'tW ' + totalW );
 	var newW = this.parent.parent.parent().width() - totalW;
+	var collapsable = this;
 	this.contenido.animate(
             { width: newW },
             1000, "",
             function(){
-	
+		if(index==collapsables.length-1){
+		    //collapsable.ocultar_titulo();
+		}
+
                 this.collapsed = false;
             }
         );
 	this.titulo.animate({left:this.contenido.width()},1000);
+
     }
 
     var cortina = this;
 
     this.titulo.click(function() {
+
         if( cortina.collapsed ) {
-            cortina.collapse();
-            cortina.collapsed = false;
+	    cortina.collapse();
+	    cortina.collapsed = false;
         }
         else {
-            cortina.expand();
-            cortina.collapsed = true;
+	    cortina.expand();
+	    cortina.collapsed = true;
         }
+
     });
+
+    this.ocultar_titulo = function() {
+	this.titulo.hide();
+    }
+
+    this.mostrar_titulo = function() {
+	this.titulo.show();
+    }
 
 
 
@@ -137,19 +170,19 @@ var Cortinas = function( parent ) {
         cortina.load(html);
         this.cortinas.push( cortina );             
         this.parent.append( cortina.div );
-
 	var tituloW = 0;
 	for(i in this.cortinas){ 
-	    tituloW += this.cortinas[i].titulo.width();
-	    console.log(tituloW);
+	    if( i < this.cortinas.length - 1 ) {
+		tituloW += this.cortinas[i].titulo.width();
+	    }
 	}
+	var newW = ( this.parent.parent().width() - tituloW );
 
 	for(i in this.cortinas){
 	    if( this.hideNext ) {}
-	    else {
-		this.cortinas[i].contenido.find('.pad_div').width(
-		    ( this.parent.parent().width() - tituloW ) * 0.9
-		)
+	    else {	
+		this.cortinas[i].contenido.width(newW);
+		this.cortinas[i].contenido.find('.pad_div').width(newW - 20);
 	    }
 	}
         
